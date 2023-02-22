574015611768e3bb2ad14033e1429846a0996475
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-22 12:15:51 +0100

    estructurando el proyecto

--- client/server-client.js
+++ client/server-client.js
@@ -5,8 +5,26 @@ const app = express();
 const server = createServer( app );
 
 app.get( '/', ( req, res ) => {
+  res.sendFile( new URL( 'login.html', import.meta.url ).pathname );
+} );
+
+app.get( '/chat', ( req, res ) => {
   res.sendFile( new URL( 'index.html', import.meta.url ).pathname );
 } );
+
+app.get( '/register', ( req, res ) => {
+  res.sendFile( new URL( 'register.html', import.meta.url ).pathname );
+} );
+
+app.get( '/logout', ( req, res ) => {
+  res.sendFile( new URL( 'login.html', import.meta.url ).pathname );
+} );
+
+app.get( '*', ( req, res ) => {
+  res.sendFile( new URL( 'NotFound.html', import.meta.url ).pathname );
+} );
+
+
 
 server.listen( 3000, () => {
   console.log( 'listening on *:3000' );
