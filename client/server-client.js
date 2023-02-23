ad0b64ec733c71d8af498dfe526731451ef117ce
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-23 23:23:34 +0100

    NotFound integrated

--- client/server-client.js
+++ client/server-client.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use( express.static( path.join( __dirname, 'public' ) ) );
 
+// rendering NotFound.html page for all routes
+app.get( '*', ( req, res ) => {
+  res.sendFile( path.join( __dirname, 'public', 'NotFound.html' ) );
+} );
+
 const PORT = 3000 || process.env.PORT;
 
 
