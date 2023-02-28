dffbf0345a5d0ad5331ed2d67740ff71fafd90fc
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-02-28 12:20:17 +0100

    crear controlador 404 aparte

--- server/routes/NotFound.js
+++ server/routes/NotFound.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get( '*', ( req, res ) => {
-  res.status( 404 ).json( { message: 'Not Found' } );
+  res.status( 404 ).json( { message: 'Not Found' } ); //TODO - Crear controlador para manejar errores
 } );
 
 
