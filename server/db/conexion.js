a9ddbc72a7f6d2973dfe5eb3997fffbb4b86679f
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-01 14:57:24 +0100

    changing url mongo

--- server/db/conexion.js
+++ server/db/conexion.js
@@ -1,10 +1,14 @@
 import { set, connect } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 set( 'strictQuery', false );
 
 const conexion = async () => {
   try {
-    await connect( 'mongodb://localhost:27017/socketChat' );
+    await connect( `${ process.env.MONGO_URI }` );
     console.log( 'Conectado a la base de datos' );
   } catch ( error ) {
     console.log( error );
